6ddd6beae0822b693a725bab17f82dab0b0ef76d
juddev <danielhuachuhuillca1@gmail.com>
2024-09-03 22:45:55 -0500

    Ejecucion correcta

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AX_PJ6o44VA3EuELofSqukIknAItz0xXUEJI1t6UyM49XWJN6USVTLMrRM-MIwJKPk4_nF1ikHj8_Hc7";
+
+const USER_SMTP = "danielhuachuhuillca1@gmail.com";
+const PASS_SMTP = "nrtv ivsj xpjp rnzj";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
